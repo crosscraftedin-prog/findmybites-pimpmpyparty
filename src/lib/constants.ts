dab00ae6a763abc78af8d08ec70b8e191ef901d5
crosscraftedin-prog <crosscraftedin@gmail.com>
2026-06-25 11:17:09 +0000

    Fix: Revert admin emails to bookingjosh only, set Listing Testing back to pending

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,7 +7,6 @@ import type { Ecosystem } from "./types";
  */
 export const ADMIN_EMAILS: string[] = [
   "bookingjosh@gmail.com",
-  "crosscraftedin@gmail.com",
 ];
 
 /** Returns true if the given email is an admin. Case-insensitive. */
